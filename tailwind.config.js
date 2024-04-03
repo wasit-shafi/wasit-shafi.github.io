722110db0e395d606d261fde8ccaa623579386ac
wasit-shafi <wasitshafi700@gmail.com>
2024-04-04 01:11:41 +0530

    added new breakpoints + updated ui layout/spacings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+// 640 - 300 = 340 / 3 = 113.33
+// 300 + 113 = 413
+// 413 + 113 = 526
 module.exports = {
 	content: ['./src/**/*.{html,ts}'],
 	theme: {
 		screens: {
-			'2xs': '300px',
-			xs: '470px',
+			'3xs': '300px',
+			'2xs': '413px',
+			xs: '526px',
 			...defaultTheme.screens,
 		},
 	},
