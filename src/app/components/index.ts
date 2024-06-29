093c45ce6331a3840091be2199ef487d7a3ad897
wasit-shafi <wasitshafi700@gmail.com>
2024-06-30 02:25:16 +0530

    removed components/shared/index.ts

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,5 +2,7 @@ export * from './about/about.component';
 export * from './contact/contact.component';
 export * from './education/education.component';
 export * from './personal-projects/personal-projects.component';
+export * from './shared/footer/footer.component';
+export * from './shared/header/header.component';
 export * from './skills/skills.component';
 export * from './work-experiences/work-experiences.component';
