b5e2bcfe689d0e3819ca428daee835fa696ca2af
wasit-shafi <wasitshafi700@gmail.com>
2024-11-04 07:17:39 +0530

    added 404-no-found fallback + black cat lottie animation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
-
-import { HomeComponent } from '@views/';
+import { HomeComponent, NotFoundComponent } from '@views/';
 
 export const routes: Routes = [
 	{
 		path: '',
 		component: HomeComponent,
+	},
+	// ** (404 not found fallback)
+
+	{
+		path: '**',
+		component: NotFoundComponent,
+		title: `Page Not Found`,
 	},
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,8 @@ module.exports = {
 			colors: {
 				black: 'rgb(00 00 00 / <alpha-value>)',
 				white: 'rgb(255 255 255 / <alpha-value>)',
+				persianGreen: 'rgb(9 166 149 / <alpha-value>)',
+				sunnyYellow: 'rgb(230 255 22 / <alpha-value>)',
 
 				bgPrimary: 'rgb(var(--color-bg-primary)  / <alpha-value>)',
 				bgPrimaryInverse:
